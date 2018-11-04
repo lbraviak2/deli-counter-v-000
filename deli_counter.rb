01e8121e09f4bd66a856b204eb1addd23d66be29
Lindsay Braviak <lbraviak@gmail.com>
2018-11-04 22:18:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,9 +9,10 @@ end
 
 def take_a_number(katz_deli, name)
   currentORDER = katz_deli.append(name)
+  LEN = currentORDER.length()
   x=0
   order = "The line is currently: "
-  while x < (currentORDER.length())
+  while x < LEN
     order << currentORDER[x+1]
     x = x+1
     return order
