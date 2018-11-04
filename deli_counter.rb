710f032d3f0988f15f855e1d3ec51c4b76cb09ab
Lindsay Braviak <lbraviak@gmail.com>
2018-11-04 22:03:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 # Write your code here.
+katz_deli = []
+
 def line(katz_deli)
-  katz_deli = []
   if katz_deli.length() == 0
     return puts "The line is currently empty."
   end
+end
+
+def take_a_number(katz_deli, name)
+  currentORDER = katz_deli.append(name)
+  return currentORDER
 end
