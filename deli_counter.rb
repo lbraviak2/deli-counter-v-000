67e42ecaf13794e4f4923c593f521df39d167b36
Lindsay Braviak <lbraviak@gmail.com>
2018-11-04 22:29:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,13 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length() == 0
     return puts "The line is currently empty."
+  else
+    x=0
+    order = "The line is currently: "
+    while x < katz_deli.length()
+      order << "#{x+1}. #{katz_deli[x+1]}"
+      x = x+1
+    end
   end
 end
 
@@ -15,7 +22,8 @@ def take_a_number(katz_deli, name)
   while x < katz_deli.length()
     order << "#{x+1}. #{katz_deli[x+1]}"
     x = x+1
-    return puts order
   end
+  
   return puts order
+  
 end
